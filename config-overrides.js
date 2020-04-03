cb774f3d15fb86687f4a83200e13e8fe06f7cecc
siaoynli <siaoynli@126.com>
2020-04-03 14:49:56 +0800

    init project

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: lixiaoyun
+ * @Company: http://hangzhou.com.cn
+ * @Github: http://github.com/siaoynli
+ * @Date: 2020-04-03 14:20:39
+ * @Description: 
+ */
+
+const {
+    override,
+    fixBabelImports
+} = require('customize-cra')
+
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css'
+    })
+)
---
+++ src/app/App.jsx
@@ -0,0 +1,22 @@
+/*
+ * @Author: lixiaoyun
+ * @Company: http://hangzhou.com.cn
+ * @Github: http://github.com/siaoynli
+ * @Date: 2020-04-03 11:25:10
+ * @Description:入口文件
+ */
+import React from 'react'
+import { Button } from 'antd'
+import './app.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary" danger>
+        Button
+      </Button>
+    </div>
+  )
+}
+
+export default App
